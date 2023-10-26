48a1e9774458506be3865c86035ab5dfa687f32b
maija-philip <maija.philip@gmail.com>
2023-10-26 15:59:24 -0400

    Initial Commit

---
+++ Raincoat/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Raincoat
+//
+//  Created by Maija Philip on 10/26/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Raincoat/RaincoatApp.swift
@@ -0,0 +1,17 @@
+//
+//  RaincoatApp.swift
+//  Raincoat
+//
+//  Created by Maija Philip on 10/26/23.
+//
+
+import SwiftUI
+
+@main
+struct RaincoatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
